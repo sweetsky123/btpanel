a09661d691629303ded2415747cc836736ef5fed
sweetsky123 <fx1024hi@gmail.com>
2025-12-20 17:04:40 +0800

    更新脚本跳过选择下载节点

--- update_panel.sh
+++ update_panel.sh
@@ -81,21 +81,22 @@ check_panel(){
 }
 
 select_node(){
-    public_file=/www/server/panel/install/public.sh
-    if [ ! -f $public_file ];then
-        download_file $public_file ${dlco}/public.sh
-    fi
+	echo -e "跳过选择下载节点"
+    #public_file=/www/server/panel/install/public.sh
+    #if [ ! -f $public_file ];then
+        #download_file $public_file ${dlco}/public.sh
+    #fi
 
-    publicFileMd5=$(md5sum ${public_file}|awk '{print $1}')
-    md5check="db0bc4ee0d73c3772aa403338553ff77"
-    if [ "${publicFileMd5}" != "${md5check}"  ]; then
-        download_file $public_file ${dlco}/public.sh
-    fi
+    #publicFileMd5=$(md5sum ${public_file}|awk '{print $1}')
+    #md5check="db0bc4ee0d73c3772aa403338553ff77"
+    #if [ "${publicFileMd5}" != "${md5check}"  ]; then
+        #download_file $public_file ${dlco}/public.sh
+    #fi
 
-    . $public_file
+    #. $public_file
 
-    download_Url=$NODE_URL
-	downloads_Url=http://io.bt.sb
+    #download_Url=$NODE_URL
+	#downloads_Url=http://io.bt.sb
 }
 
 get_version(){
